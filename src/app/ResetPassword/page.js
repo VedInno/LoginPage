4a20662eaf27d8a6064f296e26c8562cbf68b50d
VedInno <vedant.shahi@innoage.in>
2025-02-24 14:44:33 +0530

    design changes

--- src/app/ResetPassword/page.js
+++ src/app/ResetPassword/page.js
@@ -1,25 +1,27 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
-import styles from "./Login.module.css";
+import styles from "./resetpassword.module.css";
 
-export default function LoginPage() {
+export default function SignupPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(`Login attempt with Email: ${email}`);
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+    alert(`Sign-up attempt with Email: ${email}`);
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-       
-          <img src="/logo.svg" alt="Innoage Logo" className={styles.logo} />
         
-        <h2 className={styles.title}>Welcome To Innoage</h2>
-        <p className={styles.subtitle}>Sign in to your account</p>
+        <p className={styles.subtitle}>Reset Your Password</p>
         <form onSubmit={handleSubmit} className={styles.form}>
           <input
             type="email"
@@ -31,18 +33,25 @@ export default function LoginPage() {
           />
           <input
             type="password"
-            placeholder="Password"
+            placeholder="Enter Your New Password"
             className={styles.input}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-          <button type="submit" className={styles.button}>Login</button>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className={styles.input}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+          <button type="Submit" className={styles.button}>Reset</button>
         </form>
-       
         <p className={styles.footerText}>
-          Forgot Your Password?{" "}
-          <Link href="/ResetPassword" className={styles.link}>Reset Now</Link>
+          Already have an account?{" "}
+          <Link href="/login" className={styles.link}>Login</Link>
         </p>
       </div>
     </div>
